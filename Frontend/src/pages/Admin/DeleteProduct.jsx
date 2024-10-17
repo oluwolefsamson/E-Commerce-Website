827c8db094f6e338846ede27167e31bd88d24d7a
Oluwole Samson <oluwolefsamson44@gmail.com>
2024-10-17 21:03:01 +0100

    debugging cors error

--- Frontend/src/pages/Admin/DeleteProduct.jsx
+++ Frontend/src/pages/Admin/DeleteProduct.jsx
@@ -56,7 +56,7 @@ const DeleteProduct = () => {
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+      <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-8">
         {filteredProducts.length > 0 ? (
           filteredProducts.map((product) => (
             <div
